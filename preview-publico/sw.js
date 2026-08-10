6082bf13d700baba9ed87906f3a89be913630f3d
brosin420culture-cloud <brosin420culture@gmail.com>
2026-08-11 00:20:11 +0200

    Add files via upload

--- preview-publico/sw.js
+++ preview-publico/sw.js
@@ -7,7 +7,7 @@
    Si algo fuera mal, sustituye este archivo por sw-limpieza.js:
    ese desregistra el service worker y borra todo.
    =========================================================== */
-var VERSION = 'brosin-publico-6-1';
+var VERSION = 'brosin-2135-6';
 var SHELL = [
   './', './index.html', './clubs-data.js', './brosin-cluster.js', './brosin-i18n.js', './brosin-app-i18n.js', './brosin-saber.js', './brosin-paginas.js',
   './manifest.webmanifest', './formulario.html',
